b03fd58c729a911f39de7633c829750d77e0b4b9
kiselewart <kiselew.vana@yandex.ru>
2026-07-22 21:54:50 +0300

    Arrow&Separator

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
-const slider = document.querySelector('#slider');
-const sliderItems = Array.from(slider.children);
+// const slider = document.querySelector('#slider');
+// const sliderItems = Array.from(slider.children);
 
-sliderItems.forEach(function (slide, index){
-    console.log(slide);
+// sliderItems.forEach(function (slide, index){
+//     console.log(slide);
 
-    if (index !== 0) {
-        slide.classList.add('hidden');
-    }
+//     if (index !== 0) {
+//         slide.classList.add('hidden');
+//     }
 
-    slide.dataset.index = index;
+//     slide.dataset.index = index;
 
-    slide.addEventListener('click', function(){
-        slide.classList.add('hidden');
+//     slide.addEventListener('click', function(){
+//         slide.classList.add('hidden');
 
-        // let nextSlideIndex;
-        // if(index + 1 === sliderItems.length){
-        //     nextSlideIndex = 0;
-        // }else{
-        //     nextSlideIndex = index + 1;
-        // }
-        // тоже самое что if  A
-        //                    |
-        //                    |
-        //Тернарный оператор
-        let nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
+//         // let nextSlideIndex;
+//         // if(index + 1 === sliderItems.length){
+//         //     nextSlideIndex = 0;
+//         // }else{
+//         //     nextSlideIndex = index + 1;
+//         // }
+//         // тоже самое что if  A
+//         //                    |
+//         //                    |
+//         //Тернарный оператор
+//         let nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
 
-        // const nextSlideIndex = +slide.dataset.index + 1;
+//         // const nextSlideIndex = +slide.dataset.index + 1;
 
-        const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-        nextSlide.classList.remove('hidden');
+//         const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+//         nextSlide.classList.remove('hidden');
+//     })
+// });
 
-        
-    })
+document.getElementById('btn_reapet').addEventListener('click', () => {
+    window.location.href = 'index.html';
 });
+var swiper = new Swiper('.mySwiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+
+        slidesPerView: 'auto',
+        loop: true,
+        loopAdditionalSlides: 1,
+        
+        
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+          slideShadows: true,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        autoplay:{
+            delay:2000,
+            disableOnInteraction:false,
+        }
+      });
